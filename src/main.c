ca57e58679d4b5718843fd23647054bce6510576
Jace <121627078+Artzkaizen@users.noreply.github.com>
2025-09-09 11:21:54 +0200

    fix(Makefile): update file to compile on mac with new readline from brew

--- src/main.c
+++ src/main.c
@@ -2,65 +2,69 @@
 
 int g_status = 0;
 
-bool minishell_init(t_context *context, int argc, char **argv, char **env) {
-  if (!context)
-    return (false);
+bool minishell_init(t_context *context, int argc, char **argv, char **env)
+{
+    if (!context)
+        return (false);
 
-  context->argc = argc;
-  context->argv = argv;
-  context->env = env;
-  context->status = EXIT_SUCCESS;
+    context->argc = argc;
+    context->argv = argv;
+    context->env = env;
+    context->status = EXIT_SUCCESS;
 
-  printf("███╗   ███╗██╗███╗   ██╗██╗███████╗██╗  ██╗███████╗██╗     ██╗\n");
-  printf("████╗ ████║██║████╗  ██║██║██╔════╝██║  ██║██╔════╝██║     ██║\n");
-  printf("██╔████╔██║██║██╔██╗ ██║██║███████╗███████║█████╗  ██║     ██║\n");
-  printf("██║╚██╔╝██║██║██║╚██╗██║██║╚════██║██╔══██║██╔══╝  ██║     ██║\n");
-  printf(
-      "██║ ╚═╝ ██║██║██║ ╚████║██║███████║██║  ██║███████╗███████╗███████╗\n");
-  printf(
-      "╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝\n");
+    printf("███╗   ███╗██╗███╗   ██╗██╗███████╗██╗  ██╗███████╗██╗     ██╗\n");
+    printf("████╗ ████║██║████╗  ██║██║██╔════╝██║  ██║██╔════╝██║     ██║\n");
+    printf("██╔████╔██║██║██╔██╗ ██║██║███████╗███████║█████╗  ██║     ██║\n");
+    printf("██║╚██╔╝██║██║██║╚██╗██║██║╚════██║██╔══██║██╔══╝  ██║     ██║\n");
+    printf(
+        "██║ ╚═╝ ██║██║██║ ╚████║██║███████║██║  ██║███████╗███████╗███████╗\n");
+    printf(
+        "╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝\n");
 
-  return (true);
+    return (true);
 }
 
-int main(int argc, char **argv, char **env) {
-  t_context context;
-  printf("Size of void*: %zu\n", sizeof(void *));
+int main(int argc, char **argv, char **env)
+{
+    t_context context;
+    printf("Size of void*: %zu\n", sizeof(void *));
 
-  if (!minishell_init(&context, argc, argv, env))
-    return (EXIT_FAILURE);
+    if (!minishell_init(&context, argc, argv, env))
+        return (EXIT_FAILURE);
 
-  handle_signals(&context);
-  signal(SIGINT, signal_handler);
-  signal(SIGQUIT, SIG_IGN);
-  // signal(SIGTERM, signal_handler);
-  // prompt readline loop +history
-  context.input = readline("$ ");
+    handle_signals(&context);
+    signal(SIGINT, signal_handler);
+    signal(SIGQUIT, SIG_IGN);
+    // signal(SIGTERM, signal_handler);
+    // prompt readline loop +history
+    context.input = readline("minishell$ ");
 
-  while (context.input) {
-    // Tokenize input
-    add_history(context.input);
-    expand(&context);
-    t_token *tokens = lex(context.input);
-    if (!tokens)
-      (free(context.input), g_status = EXIT_FAILURE);
+    while (context.input)
+    {
+        // Tokenize input
+        add_history(context.input);
+        expand(&context);
+        t_token *tokens = lex(context.input);
+        if (!tokens)
+            (free(context.input), g_status = EXIT_FAILURE);
 
 #ifdef DEBUG
-    if (tokens)
-      print_tokens(tokens);
+        if (tokens)
+            print_tokens(tokens);
 #endif
 
-    // add_history(context.input);
-    ft_write_history(MINSHELL_DIRECTORY "/history", context.input);
-    if (ft_strncmp(context.input, "exit", 4) == 0) {
-      free(context.input);
-      exit(EXIT_SUCCESS);
+        // add_history(context.input);
+        ft_write_history(MINSHELL_DIRECTORY "/history", context.input);
+        if (ft_strncmp(context.input, "exit", 4) == 0)
+        {
+            free(context.input);
+            exit(EXIT_SUCCESS);
+        }
+        if (ft_strncmp(context.input, "history", 7) == 0)
+            print_history(MINSHELL_DIRECTORY "/history");
+
+        context.input = readline("minishell$ ");
     }
-    if (ft_strncmp(context.input, "history", 7) == 0)
-      print_history(MINSHELL_DIRECTORY "/history");
 
-    context.input = readline("$ ");
-  }
-
-  return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,56 +6,67 @@
 /*   By: chuezeri <chuezeri@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 15:10:26 by chuezeri          #+#    #+#             */
-/*   Updated: 2025/09/06 14:21:11 by chuezeri         ###   ########.fr       */
+/*   Updated: 2025/09/09 10:57:38 by chuezeri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void handler(int sig, siginfo_t *info, void *ucontext) {
+void handler(int sig, siginfo_t *info, void *ucontext)
+{
 
-  (void)ucontext; // not used
-  printf("Caught signal %d from PID %d\n", sig, info->si_pid);
+    (void)ucontext; // not used
+    printf("Caught signal %d from PID %d\n", sig, info->si_pid);
 
-  if (sig == SIGINT) {
-    g_status = SIGINT;
-  } else if (sig == SIGQUIT) {
-    // Handle Ctrl+
-    printf("\n");
-  } else if (sig == SIGTERM) {
-    g_status = SIGTERM;
-    exit(0);
-  }
+    if (sig == SIGINT)
+    {
+        g_status = SIGINT;
+    }
+    else if (sig == SIGQUIT)
+    {
+        // Handle Ctrl+
+        printf("\n");
+    }
+    else if (sig == SIGTERM)
+    {
+        g_status = SIGTERM;
+        exit(0);
+    }
 }
 
-void handle_signals(t_context *context) {
+void handle_signals(t_context *context)
+{
 
-  static struct sigaction sa = {0};
-  sa.sa_flags = 0; // use 3-arg handler
-  sa.sa_sigaction = handler;
-  sigemptyset(&sa.sa_mask);
+    static struct sigaction sa = {0};
+    sa.sa_flags = 0; // use 3-arg handler
+    sa.sa_sigaction = handler;
+    sigemptyset(&sa.sa_mask);
 
-  int a = sigaction(SIGINT, &sa, NULL);
-  sigaction(SIGQUIT, &sa, NULL);
-  sigaction(SIGTERM, &sa, NULL);
+    int a = sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
 
-  printf("Signal handlers set up (sigaction returned %d)\n", a);
+    printf("Signal handlers set up (sigaction returned %d)\n", a);
 
-  // context.
-  (void)context;
+    // context.
+    (void)context;
 }
 
-void signal_handler(int sig) {
+void signal_handler(int sig)
+{
 
-  if (sig == SIGINT) {
+    if (sig == SIGINT)
+    {
 
-    write(STDOUT_FILENO, "\n", 1);
-    rl_on_new_line();
-    rl_redisplay();
-    rl_done = 1;
-    rl_replace_line("", 0);
-  } else if (sig == SIGTERM) {
-    g_status = SIGTERM;
-    exit(0);
-  }
+        rl_replace_line("", 0);
+        write(STDOUT_FILENO, "\n", 1);
+        rl_on_new_line();
+        rl_redisplay();
+        rl_done = 1;
+    }
+    else if (sig == SIGTERM)
+    {
+        g_status = SIGTERM;
+        exit(0);
+    }
 }
