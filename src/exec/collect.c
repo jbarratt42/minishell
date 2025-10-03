f37b1ebd93478380465d48e561c49cee25885635
Your Name <you@example.com>
2025-10-03 12:04:24 +0200

    normed collect.c

---
+++ src/exec/collect.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   collect.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarratt <jbarratt@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 11:45:07 by jbarratt          #+#    #+#             */
+/*   Updated: 2025/10/03 11:47:46 by jbarratt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	collect(int pid)
+{
+	int w_status;
+
+	if (waitpid(pid, &w_status, 0) == -1)
+	//		|| !WIFEXITED(w_status))
+	{
+			perror("collect");
+			return (-1);
+	}
+	return (WEXITSTATUS(w_status));
+}
+
+int	collect2(int pids[2])
+{
+	int	status[2];
+	int	i;
+
+	i = 1;
+	while (i >= 0)
+	{
+		if(pids[i] && pids[i] != -1)
+		{
+			status[i] = collect(pids[i]);
+			if (status[i] == -1)
+				return (-1);
+		}
+		else
+			status[i] = 0;
+		i--;
+	}
+	return(status[1]);
+}
