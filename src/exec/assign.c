3285c9f31b81bfc175630710a088c17d067d6b04
Your Name <you@example.com>
2025-10-03 14:06:20 +0200

    finished norming exec/*.c

--- src/exec/assign.c
+++ src/exec/assign.c
@@ -6,7 +6,7 @@
 /*   By: chuezeri <chuezeri@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:03:37 by chuezeri          #+#    #+#             */
-/*   Updated: 2025/09/29 16:16:38 by chuezeri         ###   ########.fr       */
+/*   Updated: 2025/10/03 14:03:10 by jbarratt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	delete_tokens(t_token **token, size_t len)
 bool	is_command(t_token *token)
 {
 	while (token && token->type < PIPE && token->type != EOF_T)
+	{
 		if (token->type >= REDIR_IN && token->type <= REDIR_APPEND)
 			token = token->next->next;
 		else if (token->type == WORD && token->value && token->value[0] == '\0')
@@ -41,6 +42,7 @@ bool	is_command(t_token *token)
 			token = token->next;
 		else
 			return (true);
+	}
 	return (false);
 }
 
@@ -48,6 +50,7 @@ bool	is_command(t_token *token)
 bool	assign(t_token **token, t_context *context)
 {
 	while (*token && (*token)->type != EOF_T)
+	{
 		if ((*token)->type == WORD && ft_strchr((*token)->value, '='))
 		{
 			context->local = set_env((*token)->value, context->local);
@@ -57,51 +60,7 @@ bool	assign(t_token **token, t_context *context)
 		}
 		else
 			token = &((*token)->next->next);
-	return (true);
-}
-
-/* process a heredoc */
-bool	heredoc(t_token *token, t_context *context)
-{
-	char	*delimiter;
-	char	*line;
-	char	*tmp_file;
-	int		fd;
-
-	if (!token || !token->next || token->next->type != WORD)
-		return (false);
-	delimiter = token->next->value;
-	tmp_file = ft_strjoin(".minishell/heredoc_", delimiter);
-	if (!tmp_file)
-		return (false);
-	fd = open(tmp_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
-		return (free(tmp_file), (false));
-	while (true)
-	{
-		line = readline("> ");
-		if (!line)
-		{
-			close(fd);
-			free(tmp_file);
-			return (false);
-		}
-		if (ft_strcmp(line, delimiter) == 0)
-		{
-			free(line);
-			break ;
-		}
-		write(fd, line, ft_strlen(line));
-		write(fd, "\n", 1);
-		free(line);
 	}
-	close(fd);
-	if (context->open[0] > 2)
-		close(context->open[0]);
-	context->open[0] = open(tmp_file, O_RDONLY);
-	if (context->open[0] == -1)
-		return (free(tmp_file), false);
-	free(tmp_file);
 	return (true);
 }
 
@@ -109,6 +68,7 @@ bool	heredoc(t_token *token, t_context *context)
 bool	redirect(t_token **token, t_context *context)
 {
 	while (*token && (*token)->type < PIPE && (*token)->type != EOF_T)
+	{
 		if ((*token)->type >= REDIR_IN && (*token)->type <= REDIR_APPEND)
 		{
 			if (!reassign_fd(*token, context))
@@ -120,5 +80,6 @@ bool	redirect(t_token **token, t_context *context)
 		}
 		else
 			token = &((*token)->next);
+	}
 	return (true);
 }
---
+++ src/exec/heredoc.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarratt <jbarratt@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 14:01:49 by jbarratt          #+#    #+#             */
+/*   Updated: 2025/10/03 14:04:57 by jbarratt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	cleanup_heredoc(int fd, char *tmp_file)
+{
+	close(fd);
+	free(tmp_file);
+}
+
+static bool	read_heredoc(char *tmp_file, char *delimiter)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(tmp_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		return (free(tmp_file), (false));
+	while (true)
+	{
+		line = readline("> ");
+		if (!line)
+			return (cleanup_heredoc(fd, tmp_file), false);
+		if (ft_strcmp(line, delimiter) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		free(line);
+	}
+	close(fd);
+	return (true);
+}
+
+/* process a heredoc */
+bool	heredoc(t_token *token, t_context *context)
+{
+	char	*delimiter;
+	char	*tmp_file;
+
+	if (!token || !token->next || token->next->type != WORD)
+		return (false);
+	delimiter = token->next->value;
+	tmp_file = ft_strjoin(".minishell/heredoc_", delimiter);
+	if (!tmp_file)
+		return (false);
+	read_heredoc(tmp_file, delimiter);
+	if (context->open[0] > 2)
+		close(context->open[0]);
+	context->open[0] = open(tmp_file, O_RDONLY);
+	if (context->open[0] == -1)
+		return (free(tmp_file), false);
+	free(tmp_file);
+	return (true);
+}
